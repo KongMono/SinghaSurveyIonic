b00a5804d94353a7dc21de0b86918021b44c53ea
Freeze <freeze_yo@hotmail.com>
2017-08-21 14:50:07 +0700

    update check select all province and check select all ampher

--- src/pages/page-setting/announce-change-area/announce-change-area.ts
+++ src/pages/page-setting/announce-change-area/announce-change-area.ts
@@ -55,84 +55,116 @@ export class AnnounceChangeAreaPage {
         console.log(error);
       });
   }
-  
-    selectOption(action) {
-      let title, listSelectOption, keyOption, indexSelect;
-      if (action == 'level') {
-        title = 'เลือกเลเวล';
-        listSelectOption = this.optionUserData.level;
-        keyOption = 'level_name';
-        indexSelect = this.indexLevel;
-      } else if (action == 'province') {
-        title = 'เลือกจังหวัด';
-        listSelectOption = this.optionUserData.province;
-        keyOption = 'province_th';
-        indexSelect = this.indexProvince;
-      } else if (action == 'ampher') {
-        title = 'เลือกอำเภอ';
-        listSelectOption = this.optionUserData.province[this.indexProvince].ampher;
-        keyOption = 'ampher_th';
-        indexSelect = this.indexAmpher;
-      }
-      this.navCtrl.push('ListSelectOptionPage', { action: action, title: title, option: listSelectOption, key: keyOption, indexSelect: indexSelect, callback: this.selectOptionCallback }, { animate: true, animation: 'transition', direction: 'forward' });
+
+  selectOption(action) {
+    let title, listSelectOption, keyOption, indexSelect;
+    if (action == 'level') {
+      title = 'เลือกlevel';
+      listSelectOption = this.optionUserData.level;
+      keyOption = 'level_name';
+      indexSelect = this.indexLevel;
+    } else if (action == 'province') {
+      title = 'เลือกจังหวัด';
+      listSelectOption = this.optionUserData.province;
+      keyOption = 'province_th';
+      indexSelect = this.indexProvince;
+    } else if (action == 'ampher') {
+      title = 'เลือกอำเภอ';
+      listSelectOption = this.optionUserData.province[this.indexProvince].ampher;
+      keyOption = 'ampher_th';
+      indexSelect = this.indexAmpher;
     }
-  
-    selectOptionCallback = (_params) => {
-      return new Promise(resolve => {
-        if (_params.action == 'level') {
-          this.indexLevel = _params.indexSelect;
-          resolve();
-        } else if (_params.action == 'province') {
-          if (this.indexProvince != _params.indexSelect) {
-            this.indexAmpher = 0;
-          }
-          this.indexProvince = _params.indexSelect;
-          resolve();
-        } else if (_params.action == 'ampher') {
-          this.indexAmpher = _params.indexSelect;
-          resolve();
-        } else {
-          resolve();
+    this.navCtrl.push('ListSelectOptionPage', { action: action, title: title, option: listSelectOption, key: keyOption, indexSelect: indexSelect, callback: this.selectOptionCallback }, { animate: true, animation: 'transition', direction: 'forward' });
+  }
+
+  selectOptionCallback = (_params) => {
+    return new Promise(resolve => {
+      if (_params.action == 'level') {
+        this.indexLevel = _params.indexSelect;
+        resolve();
+      } else if (_params.action == 'province') {
+        if (this.indexProvince != _params.indexSelect) {
+          this.indexAmpher = 0;
         }
-      });
-    }
+        this.indexProvince = _params.indexSelect;
+        resolve();
+      } else if (_params.action == 'ampher') {
+        this.indexAmpher = _params.indexSelect;
+        resolve();
+      } else {
+        resolve();
+      }
+    });
+  }
 
   addArea() {
     console.log(this.optionUserData.province[this.indexProvince].province_th);
     console.log(this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th);
     if (this.selectArea.length > 0) {
-      let selectArea = this.selectArea.filter(
-        objSelectArea => objSelectArea.province.province_id == this.optionUserData.province[this.indexProvince].province_id);
-      console.log(selectArea);
-      if (selectArea[0]) {
-        console.log('duplicate province');
-        let province = selectArea[0].province.ampher.filter(
-          objProvince => objProvince.ampher_id == this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id);
-        console.log(province);
-        let duplicateAmpher = false;
-        if (province[0]) {
-          console.log('duplicate ampher');
-          duplicateAmpher = true;
-        }
-        if (!duplicateAmpher) {
-          let ampher = {
-            'ampher_id': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id,
-            'ampher_th': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th
-          }
-          selectArea[0].province.ampher.push(ampher);
-        }
-      } else {
+      if (this.selectArea[0].province.province_id == '99') {
+        this.selectArea = [];
+      }
+      if (this.optionUserData.province[this.indexProvince].province_id == '99') {
+        this.selectArea = [];
         let area = {
           'province': {
             'province_id': this.optionUserData.province[this.indexProvince].province_id,
             'province_th': this.optionUserData.province[this.indexProvince].province_th,
-            'ampher': [{
-              'ampher_id': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id,
-              'ampher_th': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th
-            }]
+            'ampher': []
           }
         };
         this.selectArea.push(area);
+      } else {
+        let selectArea = this.selectArea.filter(
+          objSelectArea => objSelectArea.province.province_id == this.optionUserData.province[this.indexProvince].province_id);
+        console.log(selectArea);
+        if (selectArea[0]) {
+          console.log('duplicate province');
+          if (this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id == '0') {
+            selectArea[0].province.ampher = [];
+            let ampher = {
+              'ampher_id': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id,
+              'ampher_th': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th
+            }
+            selectArea[0].province.ampher.push(ampher);
+          } else {
+            let province = selectArea[0].province.ampher.filter(
+              objProvince => objProvince.ampher_id == this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id);
+            console.log(province);
+            let duplicateAmpher = false;
+            if (province[0]) {
+              console.log('duplicate ampher');
+              duplicateAmpher = true;
+            }
+            if (!duplicateAmpher) {
+              let has_ampher_all = selectArea[0].province.ampher.filter(objProvince =>
+                objProvince.ampher_id == "0"
+              );
+  
+              if (has_ampher_all[0]) {
+                this.util.showAlertDialog("คุณได้เลือกอำเภอทั้งหมดแล้ว กรุณาลบแล้วเลือกใหม่");
+              } else {
+                let ampher = {
+                  'ampher_id': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id,
+                  'ampher_th': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th
+                }
+                selectArea[0].province.ampher.push(ampher);
+              }
+            }
+          }
+        } else {
+          let area = {
+            'province': {
+              'province_id': this.optionUserData.province[this.indexProvince].province_id,
+              'province_th': this.optionUserData.province[this.indexProvince].province_th,
+              'ampher': [{
+                'ampher_id': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_id,
+                'ampher_th': this.optionUserData.province[this.indexProvince].ampher[this.indexAmpher].ampher_th
+              }]
+            }
+          };
+          this.selectArea.push(area);
+        }
       }
     } else {
       let area = {
@@ -159,7 +191,7 @@ export class AnnounceChangeAreaPage {
     this.service.updateUser("", "", "", "")
       .then(
       (result: OptionUserModel) => {
-        this.util.hideLoading(); 
+        this.util.hideLoading();
         this.backPage();
       }, error => {
         this.util.hideLoading();
