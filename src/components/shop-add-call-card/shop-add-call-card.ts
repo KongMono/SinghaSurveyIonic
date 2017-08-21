4ebee3034b0649af2531c98f2788524d38e3a07c
Freeze <freeze_yo@hotmail.com>
2017-08-21 18:45:00 +0700

    -update format date in add contact and call card -add end date in call card -update view image and remove image in call card

--- src/components/shop-add-call-card/shop-add-call-card.ts
+++ src/components/shop-add-call-card/shop-add-call-card.ts
@@ -1,8 +1,17 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { AppUtilService } from './../../app/app.util';
+import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
+import { IAppConfig, ConfigApp } from './../../app/app.config';
+import { ActionSheetController, AlertController, App } from 'ionic-angular';
+import { PhotoViewer } from '@ionic-native/photo-viewer';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'shop-add-call-card',
-  templateUrl: 'shop-add-call-card.html'
+  templateUrl: 'shop-add-call-card.html',
+  providers: [
+    AppUtilService,
+    Camera,
+    PhotoViewer]
 })
 
 export class ShopAddCallCard {
@@ -15,11 +24,18 @@ export class ShopAddCallCard {
     value: '',
     spst_no: '',
     prq_no: '',
-    images: ''
+    images: []
   }
   endPoint = 'http://128.199.72.29/';
 
-  constructor() {
+  constructor(
+    public app: App,
+    public util: AppUtilService,
+    public actionSheetCtrl: ActionSheetController,
+    private camera: Camera,
+    private alertCtrl: AlertController,
+    private photoViewer: PhotoViewer,
+    @Inject(ConfigApp) private config: IAppConfig) {
 
   }
 
@@ -41,9 +57,71 @@ export class ShopAddCallCard {
 
   }
 
+  getImagePath(images): string {
+    let endpoint
+    if (this.config.isProduction) {
+      endpoint = this.config.endpoint_production;
+    } else {
+      endpoint = this.config.endpointUpload;
+    }
+    return endpoint + images;
+  }
+
+  viewImage(index) {
+    let actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          icon: 'ios-image',
+          text: 'ดูรูป',
+          handler: () => {
+            this.app.navPop().then(() => {
+              let endpoint
+              if (this.config.isProduction) {
+                endpoint = this.config.endpoint_production;
+              } else {
+                endpoint = this.config.endpointUpload;
+              }
+              this.photoViewer.show(endpoint + this.inputShopAddCallCardData.images[index]);
+            });
+          }
+        }, {
+          icon: '_icon-trash',
+          text: 'ลบ',
+          handler: () => {
+            this.app.navPop().then(() => {
+              this.confirmRemoveImage(index);
+            });
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  confirmRemoveImage(index) {
+    let alert = this.alertCtrl.create({
+      title: 'ต้องการลบรูปภาพ?',
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'ตกลง',
+          handler: () => {
+            this.inputShopAddCallCardData.images.splice(index, 1);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
   save() {
     // if (this.inputShopAddCallCardData.start_date && this.inputShopAddCallCardData.end_date && this.inputShopAddCallCardData.value && this.inputShopAddCallCardData.spst_no && this.inputShopAddCallCardData.prq_no && this.inputShopAddCallCardData.images) {
-    if (this.inputShopAddCallCardData.start_date || this.inputShopAddCallCardData.end_date && this.inputShopAddCallCardData.value && this.inputShopAddCallCardData.spst_no && this.inputShopAddCallCardData.prq_no || this.inputShopAddCallCardData.images) {
+    if (this.inputShopAddCallCardData.start_date && this.inputShopAddCallCardData.end_date && this.inputShopAddCallCardData.value && this.inputShopAddCallCardData.spst_no && this.inputShopAddCallCardData.prq_no || this.inputShopAddCallCardData.images) {
       if (this.index != null || this.index != undefined) {
         this.data.callcard[this.index].start_date = this.inputShopAddCallCardData.start_date;
         this.data.callcard[this.index].end_date = this.inputShopAddCallCardData.end_date;
