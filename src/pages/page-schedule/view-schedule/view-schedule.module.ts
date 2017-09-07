cb910b8feef73eb5d9a7774c33b561b4446f7a27
Freeze <freeze_yo@hotmail.com>
2017-09-07 19:37:51 +0700

    -add layout calendar view-schedule -add lib intl and ionic2-calendar

--- src/pages/page-schedule/view-schedule/view-schedule.module.ts
+++ src/pages/page-schedule/view-schedule/view-schedule.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ViewSchedulePage } from "./view-schedule";
+import { NgCalendarModule  } from 'ionic2-calendar';
 
 @NgModule({
     declarations: [
         ViewSchedulePage
     ],
     imports: [
-        IonicPageModule.forChild(ViewSchedulePage)
+        IonicPageModule.forChild(ViewSchedulePage),
+        NgCalendarModule
     ],
     exports: [
         ViewSchedulePage
